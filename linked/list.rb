1fce6b80bef8be03cd6916390a64b162e31f5388
Alex Reyes <alejandroereyes@gmail.com>
2016-08-12 08:46:34 -0500

    removed using an array to hold list and will build list using only next_node reference

--- linked/list.rb
+++ linked/list.rb
@@ -1,46 +1,30 @@
 require 'pry'
 require_relative 'node'
-require 'forwardable'
 module Linked
   class List
-    extend Forwardable
-    attr_accessor :list
-    delegate [:empty?, :size] => :list
-    # list will contain nodes as elements, each node contains its own value &
-    # a next_node pointer that is equal to the following node's index
+    attr_reader :head, :tail
 
     def initialize
-      @list = []
+      @head = nil
+      @tail = nil
     end
 
     def append(node)
       # adds node to end of list
-      empty_list_will_insert_this node
+      if_empty_make_head_n_tail_this node
     end
 
     def prepend(node)
       # adds node to start of list
-      empty_list_will_insert_this node
-    end
-
-    def head
-      # first node in list
-      list.first
+      if_empty_make_head_n_tail_this node
     end
 
     def tail
-      # last node in list
-      list.last
-    end
-
-    def at(index)
-      # node at the given index
-      list[index]
+     head && @tail.nil? ? head : @tail
     end
 
     def pop
       # removes last node
-      list.pop if size == 1
     end
 
     def contains?(value)
@@ -55,37 +39,35 @@ module Linked
       # formats a string of the list: (value) -> (value) -> (value) -> nil
     end
 
+    def empty?
+      head.nil? && tail.nil?
+    end
+
     def insert_at(index)
-      # inserts the value at the given index
-      empty_list_will_insert_this node
+
     end
 
     def remove_at(index)
-      # removes the node at the given index
+
     end
 
-    def empty_list_will_insert_this(node)
-      list << node if empty?
-    end
-
-    def each(&block)
-      (0...list.length).each do |i|
-        next_item = list[i].next_node
-        if list[i] == head
-          yield list[i]
-        else
-          yield list[next_item]
-        end
+    def if_empty_make_head_n_tail_this(node)
+      if empty? || head && !tail
+        @head = node
+        @tail = node
+        return node
       end
+      false
     end
   end
 end
 
 linky = Linked::List.new
-new_arr =  (1...11).to_a.each_with_object([]) do |n, arr|
-  arr << Linked::Node.new(n, n)
+letters = ['a', 'f', 'x', 'o', 'n']
+letters_arr =  letters.each_with_object([]) do |letter, arr|
+  arr << Linked::Node.new(letter)
 end
-linky.list = new_arr
+
 puts "#{linky.inspect}"
 
 # 1.times do
